d698231e0b075f5cf6cc325d01db831452e71a1a
Leo Alves <leoalves0698@gmail.com>
2023-02-24 16:07:32 -0300

    fix: corrigido tela subir ao clica para revelar senha

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,23 +1,24 @@
-const eye = document.querySelector(".eye")
+const buttonEye = document.querySelector(".eye")
 const password = document.getElementById("password")
 const hidePassword = document.getElementById("hide-password")
 const showPassword = document.getElementById("show-password")
 
-function showHidePassword() {
+function showHidePassword(event) {
     if(password.type === 'password') {
         password.setAttribute('type', 'text')
         hidePassword.style.display = 'block'
-        eye.classList.remove('eye')
-        eye.classList.add('eye-slash')
+        buttonEye.classList.remove('eye')
+        buttonEye.classList.add('eye-slash')
         showPassword.style.display = 'none'
     } else {
         password.setAttribute('type', 'password')
-        eye.classList.remove('eye-slash')
         showPassword.style.display = 'block'
-        eye.classList.remove('eye-slash')
-        eye.classList.add('eye')
+        buttonEye.classList.remove('eye-slash')
+        buttonEye.classList.add('eye')
         hidePassword.style.display = 'none'
     }
+
+    document.querySelector("form").addEventListener("submit", (event) => { event.preventDefault()})
 }
 
-eye.addEventListener('click', showHidePassword)
+buttonEye.addEventListener('click', showHidePassword)
